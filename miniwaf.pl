751d605f6851f2030af455618dfbea8bc7499895
itsusony <meng.xiangliang1985@gmail.com>
2018-04-21 15:01:53 +0900

    add dry_run mode

--- miniwaf.pl
+++ miniwaf.pl
@@ -7,7 +7,7 @@ use constant {
     NGINX_ERROR_LOG => '/usr/local/nginx/logs/error.log',
     NGINX_DENY_CONF => '/usr/local/nginx/conf/deny.conf',
     NGINX_RELOAD => '/usr/local/nginx/sbin/nginx -s reload',
-    ILLEGALS => [qw/phpmyadmin wp-login\.php CoordinatorPortType/],
+    ILLEGALS => [qw/phpmyadmin wp-login\.php CoordinatorPortType azenv\.php/],
 };
 
 unless (-r NGINX_DENY_CONF) {
@@ -15,6 +15,11 @@ unless (-r NGINX_DENY_CONF) {
     print $ndc_fp "# miniwaf\n";
     close $ndc_fp;
 }
+
+my $opt = $ARGV[0] // '';
+my $dry_run = $opt eq 'dry_run' ? 1 : 0;
+
+print "start dry_run\n" if $dry_run;
 
 my $illegals = ILLEGALS;
 
@@ -27,9 +32,13 @@ close $fp;
 sub append_deny {
     my $_ip = shift or return;
     $map_of_ips->{$_ip} = 1;
-    open my $_fp, '>> ' . NGINX_DENY_CONF;
-    print $_fp sprintf 'deny %s;'."\n", $_ip;
-    close $_fp;
+    unless ($dry_run) {
+        open my $_fp, '>> ' . NGINX_DENY_CONF;
+        print $_fp sprintf 'deny %s;'."\n", $_ip;
+        close $_fp;
+    } else {
+        print $_ip."\n";
+    }
 }
 
 sub judge {
@@ -40,15 +49,17 @@ sub judge {
     my $found = 0;
     for my $il (@$illegals) {
         if ($log =~ /$il/i) {
+            print "block reason: $il\n" if $dry_run;
             $found = 1;
             append_deny($ip);
-            &$callback($ip) if $callback;
+            &$callback($ip) if $callback && !$dry_run;
         }
     }
     $found;
 }
 
 #precheck error.log
+print "scan error.log ... \n" if $dry_run;
 my $new_in_precheck = 0;
 for my $err_filename (NGINX_ERROR_LOG, NGINX_ERROR_LOG.".1") {
     if (open my $fp2, "<".$err_filename) {
@@ -58,8 +69,9 @@ for my $err_filename (NGINX_ERROR_LOG, NGINX_ERROR_LOG.".1") {
         close $fp2;
     }
 }
+print "finished\n" if $dry_run;
 
-system NGINX_RELOAD if $new_in_precheck;
+system NGINX_RELOAD if $new_in_precheck && !$dry_run;
 
 #listen error.log
 my $file = File::Tail->new(
@@ -67,6 +79,11 @@ my $file = File::Tail->new(
   interval    => 1,
   maxinterval => 5,
 );
+
+if ($dry_run) {
+    print "dry_run finished\n";
+    exit(0);
+}
 
 while (my $log = $file->read) {
     judge($log, sub {
